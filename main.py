d448c0d5c1b4a1fb70587755b42101303ab7c0ef
rohitpant9 <66076424+rohitpant9@users.noreply.github.com>
2020-05-31 00:51:17 +0530

    first python code

---
+++ main.py
@@ -0,0 +1,18 @@
+print("hello world")
+print(5+6 )
+
+print("rohit pant is good boy")
+   #what is wrong here .....
+import math
+
+print(math.gcd(4,5))
+
+a = 32
+b = "Harry"
+c= 78.98
+d= 3
+print(a+c)
+print(a+d)
+print(d+c)
+
+
